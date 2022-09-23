395d7c139f04587348d67bad60d56309cd8c48bb
Nicolas GOEMAN <nicolas.goemn@gmail.com>
2022-09-23 09:26:59 +0200

    dev: fix pylint erros

--- tests/test_checkio.py
+++ tests/test_checkio.py
@@ -1,31 +1,46 @@
+"""
+Pytest tests file
+"""
 from dev.checkio import *
 
 def test_mult_two() -> None:
+    """_summary_
+    """
     assert mult_two(3, 2) == 6
     assert mult_two(0, 1) == 0
 
 def test_first_word() -> None:
+    """_summary_
+    """
     assert first_word("Hello world") == "Hello"
     assert first_word("a word") == "a"
     assert first_word("greeting from CheckiO Planet") == "greeting"
     assert first_word("hi") == "hi"
 
 def test_is_acceptable_password() -> None:
-    assert is_acceptable_password("short") == False
-    assert is_acceptable_password("muchlonger") == True
-    assert is_acceptable_password("ashort") == False
+    """_summary_
+    """
+    assert is_acceptable_password('short') is False
+    assert is_acceptable_password('muchlonger') is True
+    assert is_acceptable_password('ashort') is False
 
 def test_number_length() -> None:
+    """_summary_
+    """
     assert number_length(10) == 2
     assert number_length(0) == 1
     assert number_length(4) == 1
     assert number_length(44) == 2
 
 def test_most_frequent() -> None:
+    """_summary_
+    """
     assert most_frequent(["a", "b", "c", "a", "b", "a"]) == "a"
     assert most_frequent(["a", "a", "bi", "bi", "bi"]) == "bi"
 
 def test_backward_string() -> None:
+    """_summary_
+    """
     assert backward_string('val') == 'lav'
     assert backward_string('') == ''
     assert backward_string('ohho') == 'ohho'
